52dc0d14864369924b1ec7789f83f40cd2d8d72d
Ronik22 <ronikbhattacharjee22112000@gmail.com>
2021-01-31 00:46:55 +0530

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -3,11 +3,7 @@ from django.forms import fields, widgets
 from .models import Post, Comment
 
 class CommentForm(forms.ModelForm):
+    body = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control custom-txt','cols':'40','rows':'3'}), label='')
     class Meta:
         model = Comment
-        fields = ['body']
-
-        widgets = {
-
-            'body': forms.Textarea(attrs={'class':'form-control'}),
-        }
+        fields = ['body',]
