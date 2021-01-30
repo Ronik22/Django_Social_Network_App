4df62268a088b679f3f20f3cd04eb1ba8ce1ec3e
Ronik22 <ronikbhattacharjee22112000@gmail.com>
2021-01-31 00:48:09 +0530

    ajax like, comment, reply added

--- blog/urls.py
+++ blog/urls.py
@@ -1,20 +1,20 @@
 from django.urls import path
 from . import views
-from .views import AllSaveView, PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, SaveView, UserPostListView, LikeView,LikeCommentView, posts_of_following_profiles, add_comment, AllLikeView
+from .views import AllSaveView, PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, SaveView, UserPostListView, LikeView,LikeCommentView, posts_of_following_profiles,  AllLikeView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('feed/', posts_of_following_profiles, name='posts-follow-view'),
     path('post/user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/', PostDetailView, name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/like/', LikeView, name='post-like'),
+    path('post/like/', LikeView, name='post-like'),
     path('liked-posts/', AllLikeView, name='all-like'),
     path('post/<int:pk>/save/', SaveView, name='post-save'),
     path('saved-posts/', AllSaveView, name='all-save'),
-    path('post/<int:pk>/comment/', add_comment, name='post-comment'),
+    # path('post/<int:pk>/comment/', add_comment, name='post-comment'),
     path('post/<int:id1>/comment/<int:id2>/like/', LikeCommentView, name='comment-like'),
     path('about/', views.about, name='blog-about'),
     path('search/', views.search, name='search'),
--- blog/views.py
+++ blog/views.py
@@ -6,11 +6,13 @@ from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Comment, Post
 from .forms import CommentForm
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from users.models import Profile
 from itertools import chain
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.core.paginator import Paginator
+from django.template.loader import render_to_string
 
 
 def home(request):
@@ -21,6 +23,7 @@ def home(request):
 
 @login_required
 def posts_of_following_profiles(request):
+
     profile = Profile.objects.get(user = request.user)
     users = [user for user in profile.following.all()]
     posts = []
@@ -33,12 +36,14 @@ def posts_of_following_profiles(request):
     posts.append(my_posts)
     if len(posts)>0:
         qs = sorted(chain(*posts), reverse=True, key=lambda obj:obj.date_posted)
+  
     return render(request,'blog/feeds.html',{'profile':profile,'posts':qs})
 
 
 @login_required
-def LikeView(request, pk):
-    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+def LikeView(request):
+    # post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    post = get_object_or_404(Post, id=request.POST.get('id'))
     liked = False
     if post.likes.filter(id=request.user.id).exists():
         post.likes.remove(request.user)
@@ -46,7 +51,17 @@ def LikeView(request, pk):
     else:
         post.likes.add(request.user)
         liked = True
-    return HttpResponseRedirect(reverse('post-detail', args=[str(pk)]))
+
+    context = {
+        'post':post,
+        'total_likes':post.total_likes(),
+        'liked':liked,
+    }
+
+    if request.is_ajax():
+        html = render_to_string('blog/like_section.html',context, request=request)
+        return JsonResponse({'form':html})
+    # return HttpResponseRedirect(post.get_absolute_url())
 
 @login_required
 def SaveView(request, pk):
@@ -93,42 +108,138 @@ class UserPostListView(ListView):
         return Post.objects.filter(author=user).order_by('-date_posted')
 
 
-class PostDetailView(DetailView):
-    model = Post
+# class PostDetailView(DetailView):
+#     model = Post
 
-    def get_context_data(self, *args,**kwargs):
-        context = super(PostDetailView, self).get_context_data()
-        stuff = get_object_or_404(Post, id=self.kwargs['pk'])
-        total_likes = stuff.total_likes()
-        total_saves = stuff.total_saves()
-        total_comments = stuff.comments.all()
+#     def get_context_data(self, *args,**kwargs):
+#         context = super(PostDetailView, self).get_context_data()
+#         stuff = get_object_or_404(Post, id=self.kwargs['pk'])
+#         total_likes = stuff.total_likes()
+#         total_saves = stuff.total_saves()
+#         total_comments = stuff.comments.all()
 
-        tcl={}
-        for cmt in total_comments:
-            total_clikes = cmt.total_clikes()
-            cliked = False
-            if cmt.likes.filter(id=self.request.user.id).exists():
-                cliked = True
+#         tcl={}
+#         for cmt in total_comments:
+#             total_clikes = cmt.total_clikes()
+#             cliked = False
+#             if cmt.likes.filter(id=self.request.user.id).exists():
+#                 cliked = True
 
-            tcl[cmt.id] = cliked
-        context["clikes"]=tcl
-
-
-        liked = False
-        if stuff.likes.filter(id=self.request.user.id).exists():
-            liked = True
-        context["total_likes"]=total_likes
-        context["liked"]=liked
+#             tcl[cmt.id] = cliked
+#         context["clikes"]=tcl
 
 
-        saved = False
-        if stuff.saves.filter(id=self.request.user.id).exists():
-            saved = True
-        context["total_saves"]=total_saves
-        context["saved"]=saved
+#         liked = False
+#         if stuff.likes.filter(id=self.request.user.id).exists():
+#             liked = True
+#         context["total_likes"]=total_likes
+#         context["liked"]=liked
 
-        return context
 
+#         saved = False
+#         if stuff.saves.filter(id=self.request.user.id).exists():
+#             saved = True
+#         context["total_saves"]=total_saves
+#         context["saved"]=saved
+
+#         return context
+    
+
+#     def render_to_response(self, context, **response_kwargs):
+#         if self.request.is_ajax():
+#             html = render_to_string('blog/like_section.html',context, request=self.request)
+#             return JsonResponse({'form':html})
+#             # return JsonResponse('Success',safe=False, **response_kwargs)
+#         else:
+#             return super(PostDetailView,self).render_to_response(context, **response_kwargs)
+
+
+def PostDetailView(request,pk):
+
+    stuff = get_object_or_404(Post, id=pk)
+    total_likes = stuff.total_likes()
+    total_saves = stuff.total_saves()
+    # total_comments = Comment.objects.filter(post=stuff, reply=None).order_by('-id')
+    total_comments = stuff.comments.all().filter(reply=None).order_by('-id')
+    total_comments2 = stuff.comments.all().order_by('-id')
+
+    context = {}
+
+
+    if request.method == "POST":
+        comment_qs = None
+        comment_form = CommentForm(request.POST or None)
+        if comment_form.is_valid():
+            form = request.POST.get('body')
+            reply_id = request.POST.get('comment_id')
+            if reply_id:
+                comment_qs = Comment.objects.get(id=reply_id)
+            
+            comment = Comment.objects.create(name=request.user,post=stuff,body=form, reply=comment_qs)
+            comment.save() 
+            total_comments = stuff.comments.all().filter(reply=None).order_by('-id')
+            total_comments2 = stuff.comments.all().order_by('-id')
+    else:
+        comment_form = CommentForm()
+             
+
+    tcl={}
+    for cmt in total_comments2:
+        total_clikes = cmt.total_clikes()
+        cliked = False
+        if cmt.likes.filter(id=request.user.id).exists():
+            cliked = True
+
+        tcl[cmt.id] = cliked
+    context["clikes"]=tcl
+
+
+    liked = False
+    if stuff.likes.filter(id=request.user.id).exists():
+        liked = True
+    context["total_likes"]=total_likes
+    context["liked"]=liked
+
+
+    saved = False
+    if stuff.saves.filter(id=request.user.id).exists():
+        saved = True
+    context["total_saves"]=total_saves
+    context["saved"]=saved
+    
+    
+    
+
+    context['comment_form'] = comment_form
+
+    context['post']=stuff
+    context['comments']=total_comments
+
+    if request.is_ajax():
+        html = render_to_string('blog/comments.html',context, request=request)
+        return JsonResponse({'form':html})
+
+    return render(request, 'blog/post_detail.html', context)
+
+
+# <fieldset class="form-group">
+#     <legend class=" mb-4">Add Comment</legend>
+#     <textarea style="width: 100%;" name="body" id="cmtbody" cols="40" rows="5"></textarea>
+# </fieldset>
+# <div class="form-group">
+#     <button class="btn btn-outline-info" type="submit">Add Comment</button>
+# </div>
+
+
+# {% csrf_token %}
+# <input type="hidden" name="comment_id" value="{{comment.id}}">
+# <fieldset class="form-group">
+#     <legend class="h5 mb-4">Replies :</legend>
+#     <textarea style="width: 100%;" name="body" id="cmt-body" cols="40" rows="2"></textarea>
+# </fieldset>
+# <div class="form-group">
+#     <button class="btn btn-outline-info" type="submit">Reply</button>
+# </div>
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
@@ -139,21 +250,21 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-@login_required
-def add_comment(request, pk):
-    form = request.POST.get('body')
-    reply_id = request.POST.get('comment_id')
-    comment_qs = None
-    if reply_id:
-        comment_qs = Comment.objects.get(id=reply_id)
-    if request.method == "POST" and form:
-        user = request.user
-        post = get_object_or_404(Post, pk=pk)
+# @login_required
+# def add_comment(request, pk):
+#     form = request.POST.get('body')
+#     reply_id = request.POST.get('comment_id')
+#     comment_qs = None
+#     if reply_id:
+#         comment_qs = Comment.objects.get(id=reply_id)
+#     if request.method == "POST" and form:
+#         user = request.user
+#         post = get_object_or_404(Post, pk=pk)
         
-        comment = Comment(name=user,post=post,body=form, reply=comment_qs)
-        comment.save() 
-        return redirect('post-detail', post.id)
-    return redirect('post-detail', pk)
+#         comment = Comment(name=user,post=post,body=form, reply=comment_qs)
+#         comment.save() 
+        # return redirect('post-detail', post.id)
+    # return redirect('post-detail', pk)
 
 
 
