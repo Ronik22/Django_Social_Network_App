50b5932f7681ded770e41ce3d4bf1450667b056a
Andy Arp <bubbaandy89@gmail.com>
2023-01-02 18:12:28 -0500

    Require login everywhere

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import URLResolver, path
 
 from . import views
@@ -18,13 +19,15 @@ from .views import (
 
 urlpatterns: list[URLResolver] = [
     path("", views.first, name="firsthome"),
-    path("home/", PostListView.as_view(), name="blog-home"),
+    path("home/", login_required(PostListView.as_view()), name="blog-home"),
     path("feed/", posts_of_following_profiles, name="posts-follow-view"),
-    path("post/user/<str:username>/", UserPostListView.as_view(), name="user-posts"),
+    path(
+        "post/user/<str:username>/", login_required(UserPostListView.as_view()), name="user-posts"
+    ),
     path("post/<int:pk>/", PostDetailView, name="post-detail"),
-    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
-    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
-    path("post/new/", PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/update/", login_required(PostUpdateView.as_view()), name="post-update"),
+    path("post/<int:pk>/delete/", login_required(PostDeleteView.as_view()), name="post-delete"),
+    path("post/new/", login_required(PostCreateView.as_view()), name="post-create"),
     path("post/like/", LikeView, name="post-like"),
     path("liked-posts/", AllLikeView, name="all-like"),
     path("post/save/", SaveView, name="post-save"),
