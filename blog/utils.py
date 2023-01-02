7e4b452736d2f616d687aba56d983b15d9411c86
Andy Arp <bubbaandy89@gmail.com>
2023-01-02 16:20:38 -0500

    Create helper func to check usr verified

--- blog/utils.py
+++ blog/utils.py
@@ -1,3 +1,6 @@
+from users.models import Profile
+
+
 def is_ajax(request) -> bool:
     """
     To fix request.is_ajax() error which is deprecated in django > v3.1
@@ -9,3 +12,8 @@ def is_ajax(request) -> bool:
         _type_: boolean
     """
     return request.META.get("HTTP_X_REQUESTED_WITH") == "XMLHttpRequest"
+
+
+def is_user_verified(request):
+    userobj = Profile.objects.get(id=request.user.id)
+    return userobj.verified
