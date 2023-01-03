1e701d5d38013c5180d972f66c3a6b4e223b771b
Andy Arp <bubbaandy89@gmail.com>
2023-01-02 19:34:38 -0500

    add gunicorn debug level logging

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,3 +3,6 @@ proxy_allow_ips: str = "*"
 chdir: str = "/web/dsn/"
 workers: int = 5
 reload: bool = True
+errorlog: str = "/web/dsn/logs/gunicorn_error.log"
+loglevel: str = "debug"
+timeout: int = 300
