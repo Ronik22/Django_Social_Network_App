7d32a64270734696d3a3b97b165b71c2f12260f5
Andy Arp <bubbaandy89@gmail.com>
2023-01-04 13:33:35 -0500

    Set staticfiles in right place

--- myproject/urls.py
+++ myproject/urls.py
@@ -72,7 +72,6 @@ urlpatterns = [
     ),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
-
 if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
