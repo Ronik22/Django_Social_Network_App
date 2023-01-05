9fa11e9871e24c55475e7601ed32ceb0b96c13b2
Andy Arp <bubbaandy89@gmail.com>
2023-01-05 16:00:24 -0500

    Bugfix for notifications count

--- myproject/custom_context_processors.py
+++ myproject/custom_context_processors.py
@@ -1,20 +1,19 @@
 import logging
-from typing import Optional
+from typing import Dict, List, Optional
 
 from django.db.models.manager import BaseManager
 
 from notification.models import Notification
 
 
-def notifications_processor(request):
-    notifications_list: list[Notification] = []
+def notifications_processor(request) -> Dict[str, List[Notification]]:
+    notifications_list: List[Notification] = []
     if request.user.is_authenticated:
         notifications: Optional[BaseManager] = Notification.objects.filter(user=request.user)
 
         for notification in notifications:
-            logging.debug(f"{notification=}:{notification.is_seen=}")
             if not notification.is_seen:
+                logging.debug(f"{notification=}")
                 notifications_list.append(notification)
-    else:
-        notifications = 0
-    return {"notifications": notifications}
+
+    return {"notifications_list": notifications_list}
