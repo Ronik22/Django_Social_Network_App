bad3d54c0fed3a5474517aa5a11fecce3b857c7c
Andy Arp <bubbaandy89@gmail.com>
2023-01-09 13:42:15 -0500

    events dashboard table formatting

--- events/tables.py
+++ events/tables.py
@@ -4,7 +4,20 @@ from events.models import Event
 
 
 class EventsTable(tables.Table):
-    participants_count = tables.Column(
+    participants_count: tables.Column = tables.Column(
+        linkify=(
+            "events:public-viewparticipant",
+            {
+                "eid": tables.A("event_id"),
+            },
+        ),
+    )
+
+    event_description: tables.TemplateColumn = tables.TemplateColumn(
+        '<data-toggle="tooltip" title="{{record.event_description}}">{{record.event_description|truncatewords:5}}'  # noqa
+    )
+
+    event_name: tables.Column = tables.Column(
         linkify=(
             "events:public-viewparticipant",
             {
@@ -15,5 +28,13 @@ class EventsTable(tables.Table):
 
     class Meta:
         model = Event
-        exclude: tuple[str] = ("event_id", "event_poster")
+        exclude: tuple[str] = ("event_id", "id", "event_poster", "host_name", "event_author")
         attrs: dict[str, str] = {"class": "table"}
+        sequence = (
+            "event_name",
+            "event_start",
+            "event_end",
+            "registration_deadline",
+            "event_description",
+            "participants_count",
+        )
