28c772ec08e99dd5d96a9587ef4958781380b9ac
Andy Arp <bubbaandy89@gmail.com>
2023-01-12 12:55:18 -0500

    Fix Password Reset functionality

--- users/urls.py
+++ users/urls.py
@@ -10,24 +10,29 @@ urlpatterns: list[URLPattern] = [
     path("public-profile/<str:username>/", views.public_profile, name="public-profile"),
     path(
         "password_reset/done/",
-        auth_views.PasswordResetDoneView.as_view(
-            template_name="main/password/password_reset_done.html"
-        ),
+        auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"),
         name="password_reset_done",
     ),
     path(
         "reset/<uidb64>/<token>/",
         auth_views.PasswordResetConfirmView.as_view(
-            template_name="main/password/password_reset_confirm.html"
+            template_name="users/password_reset_confirm.html"
         ),
         name="password_reset_confirm",
     ),
     path(
         "reset/done/",
         auth_views.PasswordResetCompleteView.as_view(
-            template_name="main/password/password_reset_complete.html"
+            template_name="users/password_reset_complete.html"
         ),
         name="password_reset_complete",
     ),
-    path("password_reset/", views.password_reset_request, name="password_reset"),
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="users/password_reset.html",
+            email_template_name="users/password_reset_email.html",
+        ),
+        name="password_reset",
+    ),
 ]
