667794bd7b958f49cbf3bb80f4673b6e1f4cdffb
Andy Arp <bubbaandy89@gmail.com>
2023-02-16 20:43:07 +0000

    Update chat admin formatting

--- chat/admin.py
+++ chat/admin.py
@@ -1,8 +1,25 @@
+from typing import List
+
 from django.contrib import admin
 
 from .models import Chat, Room, Shout, ShoutBox
 
-admin.site.register(Chat)
-admin.site.register(Room)
-admin.site.register(Shout)
-admin.site.register(ShoutBox)
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display: List[str] = ["room_id", "author", "friend"]
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display: List[str] = ["room_id", "author", "friend"]
+
+
+@admin.register(Shout)
+class ShoutAdmin(admin.ModelAdmin):
+    list_display: List[str] = ["shoutbox", "author", "date"]
+
+
+@admin.register(ShoutBox)
+class ShoutBoxAdmin(admin.ModelAdmin):
+    list_display: List[str] = ["shoutbox_id", "shoutbox_name"]
